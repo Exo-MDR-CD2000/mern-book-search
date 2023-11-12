b61415bd2e522cac323234ed488dbd06cbd508c8
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-11-11 21:24:42 -0700

    modified server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,11 @@
-//import other dependencies
-
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { expressMiddleware } = require('@apollo/server-express4');
-
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
-
-
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -20,9 +14,7 @@ const server = new ApolloServer({
   resolvers,
 });
 
-
-// make new instance of ApolloServer and pass in schema data
-
+// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
 
@@ -49,4 +41,5 @@ const startApolloServer = async () => {
   });
 };
 
-startApolloServer();
+// Call the async function to start the server
+  startApolloServer();
