faca8bad482de71a8d45a3c02211c0682192f992
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-11-13 18:46:04 -0700

    commented code for automatic auth

--- server/server.js
+++ server/server.js
@@ -43,3 +43,35 @@ const startApolloServer = async () => {
 
 // Call the async function to start the server
   startApolloServer();
+
+
+
+
+
+
+
+
+
+
+
+//   import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+// import { setContext } from '@apollo/client/link/context';
+
+// const httpLink = createHttpLink({
+//     uri: 'http://localhost:3001/graphql',
+// });
+
+// const authLink = setContext((_, { headers }) => {
+//     const token = localStorage.getItem('token');
+//     return {
+//         headers: {
+//             ...headers,
+//             authorization: token ? `Bearer ${token}` : '',
+//         },
+//     };
+// });
+
+// const client = new ApolloClient({
+//     link: authLink.concat(httpLink),
+//     cache: new InMemoryCache(),
+// });
